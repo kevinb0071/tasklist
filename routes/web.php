c31bb33ac86b35514b66e0b7ba1ac6eba79f2a36
Kevin Bonds <kevin.bonds@gmail.com>
2024-03-29 21:04:08 -0500

    Created the Web and API routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix'=> '/', 'as' => 'tasks.'], function() {
+    Route::get('/',[TaskController::class,'index'])->name('index');
 });
