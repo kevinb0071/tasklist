ad6fae5e64ed819ac8b1557bea280211518c842d
Kevin Bonds <kevin.bonds@gmail.com>
2024-03-29 19:30:25 -0500

    Created the Tasklist app, added the Project and Tasks Models

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $table = 'projects';
+    public $timestamps = false;
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class);
+    }
+}
